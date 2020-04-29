7a8eca0e2dafa84af1b42e37fe66fbc01c8e6fc5
Vladimir Antipov <vladimir.antipov@t-systems.com>
2020-04-29 17:17:09 +0300

    Execute load test with special junit4 runner

--- web-app-test/src/test/kotlin/com/custom/acl/test/LoadTest.kt
+++ web-app-test/src/test/kotlin/com/custom/acl/test/LoadTest.kt
@@ -3,24 +3,19 @@ package com.custom.acl.test
 import org.jsmart.zerocode.core.domain.LoadWith
 import org.jsmart.zerocode.core.domain.TestMapping
 import org.jsmart.zerocode.core.domain.TestMappings
-import org.jsmart.zerocode.jupiter.extension.ParallelLoadExtension
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
+import org.jsmart.zerocode.core.runner.parallel.ZeroCodeMultiLoadRunner
+import org.junit.runner.RunWith
 
 @LoadWith("load_test.properties")
-@ExtendWith(ParallelLoadExtension::class)
-class LoadTest {
-    @Test
-    @TestMappings(
-        TestMapping(testClass = ScenariosTest::class, testMethod = "testGetNewsFeed"),
-        TestMapping(testClass = ScenariosTest::class, testMethod = "testGetUnpublshedFeeds"),
-        TestMapping(testClass = ScenariosTest::class, testMethod = "testGetFeedById"),
-        TestMapping(testClass = ScenariosTest::class, testMethod = "testPublishFeed"),
-        TestMapping(testClass = ScenariosTest::class, testMethod = "testEditFeed"),
-        TestMapping(testClass = ScenariosTest::class, testMethod = "testDeleteFeed"),
-        TestMapping(testClass = ScenariosTest::class, testMethod = "testDeleteNotExistingFeed"),
-        TestMapping(testClass = ScenariosTest::class, testMethod = "testEditNotExistingFeed")
-    )
-    fun test() {
-    }
-}
+@RunWith(ZeroCodeMultiLoadRunner::class)
+@TestMappings(
+    TestMapping(testClass = ScenariosTest::class, testMethod = "testGetNewsFeed"),
+    TestMapping(testClass = ScenariosTest::class, testMethod = "testGetUnpublshedFeeds"),
+    TestMapping(testClass = ScenariosTest::class, testMethod = "testGetFeedById"),
+    TestMapping(testClass = ScenariosTest::class, testMethod = "testPublishFeed"),
+    TestMapping(testClass = ScenariosTest::class, testMethod = "testEditFeed"),
+    TestMapping(testClass = ScenariosTest::class, testMethod = "testDeleteFeed"),
+    TestMapping(testClass = ScenariosTest::class, testMethod = "testDeleteNotExistingFeed"),
+    TestMapping(testClass = ScenariosTest::class, testMethod = "testEditNotExistingFeed")
+)
+class LoadTest
