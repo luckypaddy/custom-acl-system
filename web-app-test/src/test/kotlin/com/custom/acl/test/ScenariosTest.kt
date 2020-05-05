411797e3946bc16a5662bd3342b35c1c86c4d74f
Vladimir Antipov <vladimir.antipov@t-systems.com>
2020-05-05 16:16:49 +0300

    Disabled html report of load test for better performance

--- web-app-test/src/test/kotlin/com/custom/acl/test/ScenariosTest.kt
+++ web-app-test/src/test/kotlin/com/custom/acl/test/ScenariosTest.kt
@@ -3,10 +3,10 @@ package com.custom.acl.test
 import org.jsmart.zerocode.core.domain.Scenario
 import org.jsmart.zerocode.core.domain.TargetEnv
 import org.jsmart.zerocode.core.runner.ZeroCodeUnitRunner
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 
+@TargetEnv("disable_report.properties")
 @RunWith(ZeroCodeUnitRunner::class)
 class ScenariosTest {
 
